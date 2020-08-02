68ddc7b1635f900424685c62f0f446c5f3f53e3a
Michele Teixeira <michelealves.queiroz@gmail.com>
2020-08-01 21:26:32 -0300

    commit

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # aula.javascript
 Introdução ao JavaScript
+
+Aula de Lógica de Programação com JavaScript;
+Resumo de condicional, estrutura de repetição e DOM;
---
+++ js/main.js
@@ -0,0 +1,62 @@
+
+var d = new Date;
+alert(d);
+
+
+
+// var count;
+// for(count=0; count < 5; count++){
+//     alert(count);
+// }
+
+
+
+//estrutura de repeticao
+// var count = 0;
+
+// while (count < 5){
+//     console.log(count);
+//     count++;
+// }
+
+
+
+// var idade = prompt("Qual sua idade?");
+// // var idade = 20;
+// if (idade > 18){
+//     alert("Maior de Idade");
+// }else{
+//     alert("Menor de Idade");
+// }
+
+
+
+// var frutas = [{nome:"Maça", cor: "Vermelha"}, {nome:"Pera", cor: "Verde"}];
+// //console.log(fruta.nome);
+// //console.log(fruta.nome);
+// //alert("Vermelha");
+
+// console.log(frutas);
+// alert(frutas[1].nome);
+
+
+/* var fruta = {nome:"Maça", cor: "Vermelha"};
+//console.log(fruta.nome);
+console.log(fruta.nome);
+//alert("Vermelha"); */
+
+
+
+
+
+
+// var nome = "Michele Teixeira";
+// var idade = 41;
+// alert("Bem Vinda" + " " + nome + " " + "Idade" + " : "+ idade);
+
+
+// var lista = ["Laranja", "Abacate", "Goiaba"];
+// lista.push("Maça");
+// //lista.pop();
+// console.log(lista.length);
+// console.log(lista.join("-"));
